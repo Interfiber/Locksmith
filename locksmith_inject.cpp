d38f0333591613ef485d5d38801a2302e87cdd17
Interfiber <webmaster@interfiber.dev>
2024-03-30 20:43:29 -0400

    unlock hook

--- locksmith_inject.cpp
+++ locksmith_inject.cpp
@@ -2,6 +2,8 @@
 #include <funchook.h>
 #include <stdexcept>
 #include <pthread.h>
+#include <sys/file.h>
+#include <unistd.h>
 
 struct LockSmith_State {
     FILE* stream = nullptr;
@@ -15,10 +17,12 @@ typedef FILE* (*fopen_orig)(const char *pathname, const char *mode);
 typedef int (*fclose_orig)(FILE *stream);
 
 typedef int (*pthread_mutex_lock_orig)(pthread_mutex_t *mutex);
+typedef int (*pthread_mutex_unlock_orig)(pthread_mutex_t *mutex);
 
 static fopen_orig fopen_func = nullptr;
 static fclose_orig fclose_func = nullptr;
 static pthread_mutex_lock_orig pthread_mutex_lock_func = nullptr;
+static pthread_mutex_unlock_orig pthread_mutex_unlock_func = nullptr;
 
 FILE* Locksmith_fopen(const char *pathname, const char *mode) {
     LocksmithLog("Locksmith_fopen( %s, %s ): fopen passthrough opened file\n", pathname, mode);
@@ -26,40 +30,59 @@ FILE* Locksmith_fopen(const char *pathname, const char *mode) {
     return fopen_func(pathname, mode);
 }
 
-int Locksmith_MutexLock(pthread_mutex_t *mutex) {
-    LocksmithLog("Locksmith_MutexLock( void ): Mutex locked\n");
+int Locksmith_MutexUnlock(pthread_mutex_t *mutex) {
+    int res = pthread_mutex_unlock_func(mutex);
 
-    return pthread_mutex_lock_func(mutex);
+    unsigned int threadID = mutex->__data.__owner;
+    LocksmithLog("Locksmith_MutexUnlock( %i ): Mutex unlocked, owned by: %i\n", getpid(), threadID);
+
+
+    return res;
+}
+
+int Locksmith_MutexLock(pthread_mutex_t *mutex) {
+    int res = pthread_mutex_lock_func(mutex);
+
+    unsigned int threadID = mutex->__data.__owner;
+    LocksmithLog("Locksmith_MutexLock( %i ): Mutex locked, owned by: %i\n", getpid(), threadID);
+
+    return res;
 }
 
 void Locksmith_InstallHooks() {
-    LocksmithLog("Locksmith_InstallHooks( void ): Installing hooks...\n");
+    LocksmithLog("Locksmith_InstallHooks( %i ): Installing hooks...\n", getpid());
 
     funchook_t* fopenHook = funchook_create();
     funchook_t* pthreadLockHook = funchook_create();
+    funchook_t* pthreadUnlockHook = funchook_create();
 
     fopen_func = fopen;
     fclose_func = fclose;
     pthread_mutex_lock_func = pthread_mutex_lock;
+    pthread_mutex_unlock_func = pthread_mutex_unlock;
 
     // FIXME: Do error checking
     funchook_prepare(fopenHook, (void **) &fopen_func, (void *) Locksmith_fopen);
     funchook_prepare(pthreadLockHook, (void **) &pthread_mutex_lock_func, (void *) Locksmith_MutexLock);
+    funchook_prepare(pthreadUnlockHook, (void **) &pthread_mutex_unlock_func, (void *) Locksmith_MutexUnlock);
 
     int i1 = funchook_install(fopenHook, 0);
     int i2 = funchook_install(pthreadLockHook, 0);
+    int i3 = funchook_install(pthreadUnlockHook, 0);
 
 
-    if (i1 != 0 || i2 != 0) {
+    if (i1 != 0 || i2 != 0 || i3 != 0) {
         throw std::runtime_error("Failed to install function hook(s)!");
     }
 }
 
-void Locksmith_InitState() {
+int Locksmith_InitState() {
     // make sure to init this before we install hooks!
     locksmithState.stream = fopen("locksmith.log", "a+");
 
     LocksmithLog("Locksmith_InitState( void ): Opened locksmith.log for appending\n");
+
+    return 0;
 }
 
 // Loader
@@ -68,6 +91,10 @@ void Locksmith_InitState() {
  * Loads locksmith into the process by installing hooks and setting stuff up
  */
 void __attribute__((constructor)) LockSmith_Bootstrap(void) {
-    Locksmith_InitState();
+    if (Locksmith_InitState() == -1) return;
     Locksmith_InstallHooks();
+}
+
+void __attribute__((destructor)) LockSmith_Shutdown(void) {
+    fclose(locksmithState.stream);
 }
--- main.cpp
+++ main.cpp
@@ -1,69 +1,72 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string>
-#include <vector>
 #include <iostream>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <string>
+#include <unistd.h>
+#include <vector>
 
 void Locksmith_ShowHelp() {
-    printf("locksmith - Debug pthread programs\n");
-    printf("Author: Interfiber <webmaster@interfiber.dev>\n");
-    printf("Commands:\n");
-    printf("\tinject <command> : Launch the given program in a locksmith environment\n");
+  printf("locksmith - Debug pthread programs\n");
+  printf("Author: Interfiber <webmaster@interfiber.dev>\n");
+  printf("Commands:\n");
+  printf("\tinject <command> : Launch the given program in a locksmith "
+         "environment\n");
 }
 
-void Locksmith_Launch(std::string executable, std::vector<std::string> arguments) {
-    char* argv[arguments.size() + 1];
+void Locksmith_Launch(std::string executable,
+                      std::vector<std::string> arguments) {
+  char *argv[arguments.size() + 1];
 
-    int x = 1;
-    for (std::string &c : arguments) {
-        argv[x] = c.data();
-        x++;
-    }
+  int x = 1;
+  for (std::string &c : arguments) {
+    argv[x] = c.data();
+    x++;
+  }
 
-    argv[x + 1] = NULL;
+  argv[x + 1] = NULL;
 
-    char* envp[] = { "LD_PRELOAD=./build/liblocksmith_inject.so", NULL };
+  char *envp[] = {"LD_PRELOAD=./build/liblocksmith_inject.so", NULL};
 
-    if (execve(executable.c_str(), argv, envp) == -1) {
-        printf("Locksmith_Launch: Application launch failed! %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
-    }
+  if (execve(executable.c_str(), argv, envp) == -1) {
+    printf("Locksmith_Launch: Application launch failed! %s\n",
+           strerror(errno));
+    exit(EXIT_FAILURE);
+  }
 }
 
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        Locksmith_ShowHelp();
+  if (argc == 1) {
+    Locksmith_ShowHelp();
 
-        return EXIT_FAILURE;
+    return EXIT_FAILURE;
+  }
+
+  if (argc >= 2) {
+    std::vector<std::string> args;
+
+    for (int i = 0; i < argc; i++) {
+      args.push_back(argv[i]);
     }
 
-    if (argc >= 2) {
-        std::vector<std::string> args;
+    if (args[1] == "inject") {
+      std::vector<std::string> program;
 
-        for (int i = 0; i < argc; i++) {
-            args.push_back(argv[i]);
-        }
+      for (size_t i = 2; i < args.size(); i++) {
+        program.push_back(args[i]);
+      }
 
-        if (args[1] == "inject") {
-            std::vector<std::string> program;
+      std::cout << "Locksmith launching program: " << std::endl;
+      for (const std::string &str : program) {
+        std::cout << "\t" << str << std::endl;
+      }
 
-            for (size_t i = 2; i < args.size(); i++) {
-                program.push_back(args[i]);
-            }
+      std::string programName = program[0];
+      program.erase(program.begin());
 
-            std::cout << "Locksmith launching program: " << std::endl;
-            for (const std::string &str : program) {
-                std::cout << "\t" << str << std::endl;
-            }
+      Locksmith_Launch(programName, program);
 
-            std::string programName = program[0];
-            program.erase(program.begin());
-
-            Locksmith_Launch(programName, program);
-
-            return 0;
-        }
+      return 0;
     }
+  }
 }
